b18840687e819bdcd1cd07d7d711a8d7bb163e57
Bruno Piaui <brunopiaui@gmail.com>
2020-05-08 20:05:05 +0100

    First commit

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from "vue";
+import BrField from "@/BrField.vue";
+
+export default Vue.extend({
+  name: "ServeDev",
+  components: {
+    BrField
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <br-field />
+  </div>
+</template>
